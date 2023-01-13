4c02da67c2673172eac470c2b5fc6821a187a996
Yuma-Satake <satous.y@icloud.com>
2023-01-13 12:06:27 +0900

    分別バーの色変更・margin調整

--- frontend/ujiyamada_project_2022/src/components/molecule/CalenderCell.tsx
+++ frontend/ujiyamada_project_2022/src/components/molecule/CalenderCell.tsx
@@ -26,19 +26,19 @@ const CalenderCell:React.FC<CalenderType> = (props) => {
                 {props.item_id.map((item_id) => {
                     switch (item_id){
                         case 1:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='blue' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#5c6bc0' height='10px'></Box>)
                         case 2:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='yellow' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#ffee58' height='10px'></Box>)
                         case 3:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='green' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#66bb6a' height='10px'></Box>)
                         case 4:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='orange' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#ffa726' height='10px'></Box>)
                         case 5:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='purple' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#ab47bc' height='10px'></Box>)
                         case 6:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='red' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#ef5350' height='10px'></Box>)
                         case 7:
-                            return(<Box key={item_id} sx={{my:0.1, mx:0.1}} borderRadius='2px' bgcolor='skyblue' height='10px'></Box>)
+                            return(<Box key={item_id} sx={{my:0.2, mx:0.1}} borderRadius='2px' bgcolor='#29b6f6' height='10px'></Box>)
                         default:
                             break;
                     }
