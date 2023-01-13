7c8bdcbcf7049a0a9cd8ff3c8cbaaf577c89a3c4
Yuma-Satake <satous.y@icloud.com>
2023-01-13 12:08:31 +0900

    分別色一覧追加

--- frontend/ujiyamada_project_2022/src/components/molecule/CalenderItemList.tsx
+++ frontend/ujiyamada_project_2022/src/components/molecule/CalenderItemList.tsx
@@ -1,47 +1,93 @@
-import { Box, Grid, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import Typography from "@mui/material/Typography";
+import { Box, Stack } from "@mui/system";
 
-const CalenderItemList = () => {
+
+const CalenderItemList:React.FC = () => {
     return(
-        <Grid2 sx={{m:3}} bgcolor='red' container>
+        <Grid2 sx={{m:2}} container>
             {itemList.map((item) => {
-                return(
-                    <Grid2 sx={{m:1}}>{item.itemName}だよ</Grid2>
-                )
+                switch(item.color){
+                    case 1:
+                        return(
+                        <Stack direction='row' key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}}/>
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 2:
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} />
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 3:
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} color='success'/>
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 4: 
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} color='warning'/>
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 5:
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} />
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 6:
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} color='error'/>
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    case 7:
+                        return(
+                        <Stack direction='row'key={item.color} sx={{m:1}}>
+                            <TaskAltIcon sx={{mr:0.8}} color='info'/>
+                            <Typography>{item.itemName}</Typography>
+                        </Stack>)
+                    default:
+                        return(<Box>エラーが発生しました</Box>)
+                }
             })}
         </Grid2>
-    )
+   )
 }
 
 export default CalenderItemList;
 
 const itemList = [
     {
-        itemName:'',
-        color:'blue'
+        itemName:'ガラス・くずびん類・陶磁器類',
+        color:1
     },
     {
-        itemName:'',
-        color:'yellow'
+        itemName:'布類・段ボール類',
+        color:2
     },
     {
-        itemName:'',
-        color:'green'
+        itemName:'資源びん',
+        color:3
     },
     {
-        itemName:'',
-        color:'orange'
+        itemName:'プラスチック',
+        color:4
     },
     {
-        itemName:'',
-        color:'purple'
+        itemName:'ペットボトル',
+        color:5
     },
     {
-        itemName:'',
-        color:'red'
+        itemName:'燃えるごみ',
+        color:6
     },
     {
-        itemName:'',
-        color:'skyblue'
+        itemName:'紙類(新聞・折込チラシ・飲料用紙パック・雑誌・雑がみ類)',
+        color:7
     }
 ]
