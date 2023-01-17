e1bf4d0d5ade6233994885f59e78562289072bd3
Yuma Satake <satous.y@icloud.com>
2023-01-17 12:05:58 +0900

    型付修正

--- frontend/ujiyamada_project_2022/src/components/router/MainRouter.tsx
+++ frontend/ujiyamada_project_2022/src/components/router/MainRouter.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "../page/ErrorPage";
 import HomePage from "../page/HomePage";
 
-const MainRouter = () => {
+const MainRouter:React.FC = () => {
     return(
         <BrowserRouter>
             <Routes>
