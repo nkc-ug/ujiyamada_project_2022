94c5af3ca9c8311b38a3c9b4fe903872806096ea
saimun15 <95845104+saimun15@users.noreply.github.com>
2023-01-17 23:26:57 +0900

    bockend code

---
+++ backend/todo-api/app/Http/Controllers/TodoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use Illuminate\Support\Facades\DB;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function divide($item_name)
+    {
+       $items = DB::table('items')->where('name', 'like', '%'.$item_name.'%')->get();
+        return json_encode($items, JSON_UNESCAPED_UNICODE);
+
+    }
+    public function getItem($item_name)
+    {
+        $items = DB::table('items')->where('name', '=', $item_name)->get();
+        return json_encode($items, JSON_UNESCAPED_UNICODE);
+
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ backend/todo-api/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SampleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/app/todo/index', [App\Http\Controllers\TodoController::class, 'index']);
+//Route::get('/api/calender', [App\Http\Controllers\TodoController::class, 'calander']);
+Route::get('/api/divide/{item_name}', [App\Http\Controllers\TodoController::class, 'divide']);
+Route::get('/api/divide/item/{item_name}', [App\Http\Controllers\TodoController::class, 'getItem']);
