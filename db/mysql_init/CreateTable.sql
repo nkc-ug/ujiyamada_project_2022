321ffb7bcd3342e5e4fe928e2ef8bd844f0401da
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-02-04 19:16:00 +0900

    CreateTableを修正

--- db/mysql_init/CreateTable.sql
+++ db/mysql_init/CreateTable.sql
@@ -5,11 +5,41 @@ CREATE TABLE separations (
 );
 
 CREATE TABLE kinds (
-    id int(3) AUTO_INCREMENT,
+    id int(3),
     name varchar(20),
     separation_id int(2),
     point varchar(50),
     PRIMARY KEY(id),
-    FOREIGN KEY separation_id,
-    REFERENCES separations
+    FOREIGN KEY(separation_id)
+    REFERENCES separations(id)
+);
+
+CREATE TABLE monthly_days (
+    area_id int(3) AUTO_INCREMENT,
+    month int(2),
+    separation_id int(2),
+    collection_day char(2),
+    PRIMARY KEY(id, month),
+    FOREIGN KEY(separation_id)
+    REFERENCES separations(id)
+);
+
+CREATE TABLE areas (
+    id int(3),
+    name varchar(20),
+    burnable_day_01 char(3),
+    burnable_day_02 char(3),
+    plastic_day char(3),
+    PRIMARY KEY(id),
+    FOREIGN KEY(id)
+    REFERENCES separations(area_id)
+);
+
+CREATE TABLE synonyms (
+    id int(4),
+    kind_id int(3),
+    name varchar(20),
+    PRIMARY KEY(id),
+    FOREIGN KEY(kind_id)
+    REFERENCES separations(id)
 );
