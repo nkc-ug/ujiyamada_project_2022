e6a343b28aaf584ba0508fe500aa30ea855b8bc5
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-02-20 12:44:24 +0900

    CreateTable.sqlの更新,InsertSeparations.sqlとの統廃合

--- db/mysql_init/CreateTable.sql
+++ db/mysql_init/CreateTable.sql
@@ -44,3 +44,22 @@ CREATE TABLE synonyms (
     FOREIGN KEY(kind_id)
     REFERENCES separations(id)
 );
+
+INSERT INTO separations
+VALUES
+    (1,'blue', 'ガラス、くずびん類、陶磁器類'),
+    (2,'yellow', '布類、段ボール類'),
+    (3,'green', '資源びん'),
+    (4,'orange', 'プラスチック製容器包装'),
+    (5,'purple', 'ペットボトル'),
+    (6,'red', '燃えるごみ'),
+    (7,'skyblue', '紙類(新聞・折込チラシ、飲料用紙パック、雑誌・雑がみ類)'),
+    (8,'light_blue', '缶・金属類'),
+    (9, NULL, '粗大ごみ'),
+    (10, NULL, '家電４品目'),
+    (11, NULL, '蛍光管'),
+    (12, NULL, 'ガレキ類'),
+    (13, NULL, '乾電池・蛍光管'),
+    (14, NULL, '布類'),
+    (15, NULL, 'ダンボール類');
+    (16, NULL, 'その他');
--- db/mysql_init/CreateTableSearch.sql
+++ db/mysql_init/CreateTableSearch.sql
@@ -1,3 +1,3 @@
-SELECT * FROM synonyms CROSS JOIN kinds
+SELECT * FROM [synonyms CROSS JOIN kinds]
 WHERE [要素名]
 LIKE [曖昧検索の条件];
