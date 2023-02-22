f67cd62e3dfe6d046a5b2764f208138b9619517d
Iyarr <8.24ridiculous@gmail.com>
2023-02-23 00:24:15 +0900

    pkの省略

--- db/mysql_init/CreateTable.sql
+++ db/mysql_init/CreateTable.sql
@@ -45,22 +45,22 @@ CREATE TABLE synonyms (
     REFERENCES separations(id)
 );
 
-INSERT INTO separations
+INSERT INTO separations (color,name)
 VALUES
-    (1,'blue', 'ガラス、くずびん類、陶磁器類'),
-    (2,'yellow', '布類、段ボール類'),
-    (3,'green', '資源びん'),
-    (4,'orange', 'プラスチック製容器包装'),
-    (5,'purple', 'ペットボトル'),
-    (6,'red', '燃えるごみ'),
-    (7,'skyblue', '紙類(新聞・折込チラシ、飲料用紙パック、雑誌・雑がみ類)'),
-    (8,'light_blue', '缶・金属類'),
-    (9, NULL, '粗大ごみ'),
-    (10, NULL, '家電４品目'),
-    (11, NULL, '蛍光管'),
-    (12, NULL, 'ガレキ類'),
-    (13, NULL, '乾電池・蛍光管'),
-    (14, NULL, '布類'),
-    (15, NULL, 'ダンボール類'),
-    (16, NULL, 'てんぷら油、または燃えるごみ'),
-    (17, NULL, 'その他');
+    ('blue', 'ガラス、くずびん類、陶磁器類'),
+    ('yellow', '布類、段ボール類'),
+    ('green', '資源びん'),
+    ('orange', 'プラスチック製容器包装'),
+    ('purple', 'ペットボトル'),
+    ('red', '燃えるごみ'),
+    ('skyblue', '紙類(新聞・折込チラシ、飲料用紙パック、雑誌・雑がみ類)'),
+    ('light_blue', '缶・金属類'),
+    ( NULL, '粗大ごみ'),
+    ( NULL, '家電４品目'),
+    ( NULL, '蛍光管'),
+    ( NULL, 'ガレキ類'),
+    ( NULL, '乾電池・蛍光管'),
+    ( NULL, '布類'),
+    ( NULL, 'ダンボール類'),
+    ( NULL, 'てんぷら油、または燃えるごみ'),
+    ( NULL, 'その他');
